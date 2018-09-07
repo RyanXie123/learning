6e776236c25dc775f28268ab47c2a528147b7345
谢汝 <xieru@qiyi.com>
2018-09-07 09:51:16 +0800

    日期分类

--- XR_Douyin/XR_Douyin/Category/NSDate+Extension.m
+++ XR_Douyin/XR_Douyin/Category/NSDate+Extension.m
@@ -10,6 +10,59 @@
 
 @implementation NSDate (Extension)
 
++ (NSString *)formatTime:(NSTimeInterval)timeInerval {
+    NSDate *date = [NSDate dateWithTimeIntervalSinceNow:timeInerval];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    
+    if ([date isToday]) {
+        if ([date isJustNow]) {
+            return @"刚刚";
+        }else {
+            formatter.dateFormat = @"HH:mm";
+            return [formatter stringFromDate:date];
+        }
+    }else {
+        if ([date isYesterDay]) {
+            formatter.dateFormat = @"昨天HH:mm";
+            return [formatter stringFromDate:date];
+        }else if ([date isCurrentWeek]){
+            formatter.dateFormat = [NSString stringWithFormat:@"%@%@",[date dateToWeekday],@"HH:mm"];
+            return [formatter stringFromDate:date];
+        }else {
+            if ([date isCurrentYear]) {
+                formatter.dateFormat = @"MM-dd HH:mm";
+            }else {
+                formatter.dateFormat = @"yy-MM-dd  HH:mm";
+            }
+            return [formatter stringFromDate:date];
+        }
+        
+        
+        
+        
+    }
+    
+
+    
+}
+
+
+- (NSString *)dateToWeekday {
+    NSArray *weekdays = [NSArray arrayWithObjects: @"", @"星期天", @"星期一", @"星期二", @"星期三", @"星期四", @"星期五", @"星期六", nil];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSTimeZone *timeZone = [[NSTimeZone alloc] initWithName:@"Asia/Shanghai"];
+    [calendar setTimeZone: timeZone];
+    NSCalendarUnit calendarUnit = NSCalendarUnitWeekday;
+    NSDateComponents *theComponents = [calendar components:calendarUnit fromDate:self];
+    return [weekdays objectAtIndex:theComponents.weekday];
+    
+}
+
+
+- (BOOL)isJustNow {
+    NSTimeInterval now = [[NSDate date] timeIntervalSince1970];
+    return (fabs(now - self.timeIntervalSince1970 ) < 60 * 2);
+}
 
 - (BOOL)isCurrentYear {
     NSCalendar *calendar = [NSCalendar currentCalendar];
@@ -28,8 +81,26 @@
 
 }
 
+- (BOOL)isCurrentWeek {
+    NSDate *nowDate = [[NSDate date]dateFormatYMD];
+    NSDate *selfDate = [self dateFormatYMD];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *cmps = [calendar components:NSCalendarUnitDay fromDate:selfDate toDate:nowDate options:0];
+    
+    return cmps.day < 7;
+    
+//    NSDateComponents *cmps = []
+    
+}
 - (BOOL)isYesterDay {
-    return NO;
+    NSDate *nowDate = [[NSDate date]dateFormatYMD];
+    NSDate *selfDate = [self dateFormatYMD];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *cmps = [calendar components:NSCalendarUnitDay fromDate:selfDate toDate:nowDate options:0];
+    return cmps.day == 1;
+    
+    
 }
 
 
